024b04e8a984fa5ba963dd95a5e90d30ff820df4
IROTORI Baroka <baroka@globodai.com>
2025-12-01 10:50:35 +0100

    fix: URL exacte OurVoice API confirmée (https://api.getourvoice.com/v1/messages)

--- lib/sms.ts
+++ lib/sms.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 import { prisma } from './prisma';
 
 // OurVoice SMS API
-// Note: Vérifiez l'URL de base exacte dans votre documentation OurVoice
-// Options possibles: https://api.getourvoice.com, https://getourvoice.com/api, etc.
 const OURVOICE_BASE_URL = process.env.OURVOICE_BASE_URL || 'https://api.getourvoice.com';
 const OURVOICE_API_URL = `${OURVOICE_BASE_URL}/v1/messages`;
 
